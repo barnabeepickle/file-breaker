bd1ef863fd88f7b00a07d66484db9f8bc9375eb8
barnabeepickle <linusrk30@gmail.com>
2025-03-14 12:42:57 -0400

    mostly finilized base splitter code

--- main.py
+++ main.py
@@ -3,15 +3,24 @@
 import os
 import argparse
 import tarfile
+import csv
 
 # func
-def file_split_bysize(input_file,chunk_size):
+def file_split(input_file,chunk_size,compress=True,build_csv=True):
     """Splits a file into smaller chunks by size.
     Args:
         input_file: Path to the input file.
         chunk_size: Maximum size of each chunk in bytes.
+        compress:   Bool, if part files should be compressed.
+        build_csv: Bool, if .csv files should be created for putting parts back together.
     """
     if not os.path.getsize(input_file)<=chunk_size or os.path.getsize(input_file)==chunk_size: 
+        if build_csv==True: # sets up .csv files that contain file names for rebuilding files
+            part_csv=open(f'{input_file}.csv','a',newline='')
+            part_index=csv.writer(part_csv,delimiter=' ',quotechar='|',quoting=csv.QUOTE_MINIMAL)
+            if compress==True:
+                tar_csv=open(f'{input_file}.tar.csv','a',newline='')
+                tar_index=csv.writer(tar_csv,delimiter=' ',quotechar='|',quoting=csv.QUOTE_MINIMAL)
         file_number=1
         with open(input_file,'rb') as file:
             while True:
@@ -21,11 +30,28 @@ def file_split_bysize(input_file,chunk_size):
                 output_file=f'{input_file}.part_{file_number}'
                 with open(output_file,'wb') as outfile:
                     outfile.write(chunk)
+                if build_csv==True:
+                    part_index.writerow([output_file])
+                if compress==True:
+                    try:
+                        tar=tarfile.open(f'{output_file}.tar','x:xz')
+                    except:
+                        tar=tarfile.open(f'{output_file}.tar','w:xz')
+                    tar.add(output_file)
+                    os.remove(output_file)
+                    if build_csv==True:
+                        tar_index.writerow([f'{output_file}.tar'])
+                    tar.close()
                 file_number+=1
+        if build_csv==True: # closes open csv files since this implementation doesn't use the with method
+            part_csv.close()
+            if compress==True:
+                tar_csv.close()
     else:
         print('File is smaller than or equal to chunk size, not splitting file')
 
 # Example usage:
-file_path='README.md' # input file path
-chunk_size_bytes=1024*1024 # 1MB
-file_split_bysize(file_path,chunk_size_bytes)
+file_path='main.py' # input file path
+# chunk_size=1024*1024*50 # 50MB i think
+chunk_size=1000
+file_split(file_path,chunk_size)
