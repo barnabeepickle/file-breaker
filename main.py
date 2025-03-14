6545940b3253d8a378b806c186a40038b6c0b0cf
barnabeepickle <linusrk30@gmail.com>
2025-03-14 11:17:56 -0400

    implemented file splitter

--- main.py
+++ main.py
@@ -1,6 +1,31 @@
-"""CLi tool for splitting files, compressing them and reassembling them"""
+"""CLI tool for splitting files, compressing them and reassembling them"""
 # imports
+import os
 import argparse
-import filesplit
 import tarfile
 
+# func
+def file_split_bysize(input_file,chunk_size):
+    """Splits a file into smaller chunks by size.
+    Args:
+        input_file: Path to the input file.
+        chunk_size: Maximum size of each chunk in bytes.
+    """
+    if not os.path.getsize(input_file)<=chunk_size or os.path.getsize(input_file)==chunk_size: 
+        file_number=1
+        with open(input_file,'rb') as file:
+            while True:
+                chunk=file.read(chunk_size)
+                if not chunk:
+                    break
+                output_file=f'{input_file}.part_{file_number}'
+                with open(output_file,'wb') as outfile:
+                    outfile.write(chunk)
+                file_number+=1
+    else:
+        print('File is smaller than or equal to chunk size, not splitting file')
+
+# Example usage:
+file_path='README.md' # input file path
+chunk_size_bytes=1024*1024 # 1MB
+file_split_bysize(file_path,chunk_size_bytes)
