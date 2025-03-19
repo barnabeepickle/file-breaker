a80e78cd7edf00670d4b4a08a48c04ed779e90cd
barnabeepickle <linusrk30@gmail.com>
2025-03-19 12:56:10 -0400

    Initial CLI implementation

--- breaker_cli.py
+++ breaker_cli.py
@@ -3,3 +3,12 @@
 import file_breaker
 import argparse
 
+parser = argparse.ArgumentParser( # most of this is just taken directly from the documentation
+                    prog='file-breaker-cli',
+                    usage='%(prog)s [options]', # smarter code than I
+                    description='A CLI interface for breaking files into segments.',
+                    epilog='"AS IS", WITHOUT WARRANTY')
+parser.add_argument('input_path')
+parser.add_argument('-b', '--break',action='store_true')
+parser.add_argument('-e', '--extract',action='store_true')
+parser.add_argument('-s', '--size')
