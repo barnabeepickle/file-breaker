31746c62c76a628b1cbb39a13d485742969d20dc
barnabeepickle <linusrk30@gmail.com>
2025-03-19 13:31:15 -0400

    finished argparse setup

--- breaker_cli.py
+++ breaker_cli.py
@@ -2,16 +2,30 @@
 # imports
 import file_breaker
 import argparse
+import pathlib
 
-# argparse parser
-parser = argparse.ArgumentParser( # most of this is just taken directly from the documentation
+# argparse setup
+path=''
+mode=''
+size=1024*1024*50
+parser=argparse.ArgumentParser( # most of this is just taken directly from the documentation
                     prog='file-breaker-cli',
                     usage='%(prog)s [options]', # smarter code than I, now don't quote me on that
                     description='A CLI interface for breaking files into segments.',
                     epilog='"AS IS", WITHOUT WARRANTY')
-# argparse arguments
-parser.add_argument('input_path',type=str,help='Path to the input file.')
-parser.add_argument('-b', '--break',action='store_true',help='If the input file should be split into parts.')
-parser.add_argument('-e', '--extract',action='store_true',help='If the input file should be reassembled')
-parser.add_argument('-s', '--size',type=int,help='Specify the size in bytes of the output file.')
+parser.add_argument('input_path',
+                    type=pathlib.Path,
+                    help='Path to the input file.',
+                    namespace=path)
+parser.add_argument('mode',
+                    type=str,
+                    help='If the input file should be broken or reassembled',
+                    namespace=mode)
+parser.add_argument('-s', '--size',
+                    type=int,
+                    help='Specify the size in bytes of the output file.',
+                    default=1024*1024*50,
+                    namespace=size)
+mode=mode.lower()
 
+# code
