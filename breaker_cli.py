2aa217f7033e93842dac1939396126ee65f9deac
barnabeepickle <linusrk30@gmail.com>
2025-03-24 10:46:16 -0400

    near complete breaker_cli.py rewrite

--- breaker_cli.py
+++ breaker_cli.py
@@ -1,38 +1,48 @@
-"""The CLI interface for file-breaker."""
+"""The CLI interface for file_breaker using typer."""
 # imports
 import file_breaker as breaker
-import argparse
-import pathlib
+import typer
+from typing_extensions import Annotated
 
-# vars
+# some vars
 default_size=1024*1024*50
-mode_key={'break':0,'rebuild':1,'gen':2}
+app=typer.Typer()
 
-# argparse has been sent to the shadow realm
+# func
+@app.command()
+def index_gen(path:str): # handles index generation
+    out=breaker.index_gen(path)
+    if out[0]==True:
+        print(f'A new part index file has been generated and has not over written the old one.')
+    if out[1]==True:
+        print(f'A new tar index file has been generated and has not over written the old one.')
 
-# code
-if mode==1 and csv==True:
-    gen_index=True
-if mode==2:
-    gen_index=True
-if gen_index==True: # handles the -c, --csv, gen_index or mode==2 args
-    overrides=breaker.index_gen(path)
-    if overrides[0]==True:
+@app.command()
+def file_break(path:str,
+               size:Annotated[int,typer.Argument()]=default_size,
+               csv:Annotated[bool,typer.Argument()]=True):
+    breaker.file_break(path,size,False,csv,True)
+
+@app.command()
+def file_join(path:str,
+              size:Annotated[int,typer.Argument()]=default_size,
+              gen:Annotated[bool,typer.Argument()]=False):
+    # index generation handling
+    out=False,False
+    if gen==True:
+        out=breaker.index_gen(path)
+    if out[0]==True:
         part_override=f'{path}.new'
-        if mode==2:
-            print(f'The new part index file has been generated with the name: {part_override}')
-    else: # setting your override to 'null' causes it to be filtered out
+    else:
         part_override='null'
-    if overrides[1]==True:
+    if out[1]==True:
         tar_override=f'{path}.new'
-        if mode==2:
-            print(f'The new tar index file has been generated with the name: {tar_override}')
-    else: # same as the other 'null' filter override
+    else:
         tar_override='null'
-    if mode==2:
-        print('The new index files have been generated correctly.') # got that feedback to the user
-if mode==0:
-    breaker.file_break(path,size,build_csv=not csv) # is this valid code?
-elif mode==1:
-    breaker.file_build(path,part_override,tar_override) # it always uses the override values 
-    # but uses the 'null' filter to make simpler code happen, uses this at your own risk
+    # builder
+    breaker.file_build(path,part_override,tar_override)
+
+# your a coder harry!
+if __name__=='__main__':
+    app()
+    # Typer is much easier than argparse and at least a little bit easier than Click
