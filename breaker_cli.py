ae7c00866bd0ca6d6e1cf6c21619b018dec0b304
barnabeepickle <linusrk30@gmail.com>
2025-03-24 11:15:36 -0400

    removed superflous code, added help and docstrings

--- breaker_cli.py
+++ breaker_cli.py
@@ -9,24 +9,38 @@ default_size=1024*1024*50
 app=typer.Typer()
 
 # func
-@app.command()
-def index_gen(path:str): # handles index generation
+@app.command(help='Generate a csv index dynamically for use in rebuilding.')
+def index_gen(path:str):
+    """Handles index generation requests from the user.
+    Args:
+        path:   The path to the files in the form of the original file name.
+    """
     out=breaker.index_gen(path)
     if out[0]==True:
         print(f'A new part index file has been generated and has not over written the old one.')
     if out[1]==True:
         print(f'A new tar index file has been generated and has not over written the old one.')
 
-@app.command()
+@app.command(help='Segments files into different sections.')
 def file_break(path:str,
                size:Annotated[int,typer.Argument()]=default_size,
-               csv:Annotated[bool,typer.Argument()]=True):
+               csv:Annotated[bool,typer.Option('--csv','-c')]=True):
+    """Handles file break requests from the user.
+    Args:
+        path:   The path to the files in the form of the original file name.
+        size:   Size of the resulting chunked files before compression.
+        csv:    If a csv index file should be created.
+    """
     breaker.file_break(path,size,False,csv,True)
 
-@app.command()
+@app.command(help='Rebuilds files that have been broken.')
 def file_join(path:str,
-              size:Annotated[int,typer.Argument()]=default_size,
-              gen:Annotated[bool,typer.Argument()]=False):
+              gen:Annotated[bool,typer.Option('--gen','-g')]=True):
+    """Handles file join requests from the user.
+    Args:
+        path:   The path to the files in the form of the original file name.
+        gen:    If the csv index file should be generated on the fly.
+        """
     # index generation handling
     out=False,False
     if gen==True:
