98dc923309eb61b69d5e261f8310d948daa63476
barnabeepickle <linusrk30@gmail.com>
2025-03-24 18:41:32 -0400

    removed action, major logo code changes

--- resources/logo.py
+++ resources/logo.py
@@ -1,11 +1,17 @@
 """Generates a logo for this project using Pillow."""
 # imports
 import PIL
-from PIL import Image,ImageDraw
+from PIL import Image,ImageDraw,ImageFont
 import os
 
+# var
+rad=60
+sub_size=(1000,1000)
+half=sub_size[0]//2
+logo_size=(sub_size[0]*3,sub_size[1])
+
 # func
-def add_corners(im, rad): # stolen from stackoverflow 11287402
+def add_corners(im, rad): # stolen from stackoverflow 11287402 # work smarter not harder
     circle = Image.new('L', (rad * 2, rad * 2), 0)
     draw = ImageDraw.Draw(circle)
     draw.ellipse((0, 0, rad * 2 - 1, rad * 2 - 1), fill=255)
@@ -17,36 +23,31 @@ def add_corners(im, rad): # stolen from stackoverflow 11287402
     alpha.paste(circle.crop((rad, rad, rad * 2, rad * 2)), (w - rad, h - rad))
     im.putalpha(alpha)
     return im
-
-def merge(im1: Image.Image, im2: Image.Image) -> Image.Image: # stolen from the pillow documentation
-    w = im1.size[0] + im2.size[0]
-    h = max(im1.size[1], im2.size[1])
-    im = Image.new("RGBA", (w, h))
-    im.paste(im1)
-    im.paste(im2, (im1.size[0], 0))
+def add_center_text(im,in_text): # not stolen, written from scratch
+    x,y=im.size
+    font=ImageFont.truetype('resources/Audiowide/Audiowide-Regular.ttf',half,0,'unic')
+    txt=ImageDraw.ImageDraw.text((x,y),in_text,'#f0ffff',font,'mm',4,'Center','rtl','None','en',0,'#f0ffff',False,12)
+    im.paste(txt,(0,0))
     return im
+    
+def square_maker(filename,hex,s_size,s_rad,in_text=''):
+    with Image.new('RGBA',s_size,hex) as square:
+        square=add_corners(square,s_rad)
+        if not in_text=='':
+            square=add_center_text(square,in_text)
+        square.save(f'{filename}.png')
 
-# var
-rad=50
-size=(1000,1000)
-logo_size=(size[0]*3,size[1])
 # code
-with Image.new('RGBA',size,'#818589') as square:
-    square=add_corners(square,rad)
-    square.save('square1.png')
-with Image.new('RGBA',size,'#7a7a7a') as square:
-    square=add_corners(square,rad)
-    square.save('square2.png')
-with Image.new('RGBA',size,'#737373') as square:
-    square=add_corners(square,rad)
-    square.save('square3.png')
+square_maker('square1','#818589',sub_size,rad,'')
+square_maker('square2','#7a7a7a',sub_size,rad,'')
+square_maker('square3','#737373',sub_size,rad,'')
 square1=Image.open('square1.png')
 square2=Image.open('square2.png')
 square3=Image.open('square3.png')
 with Image.new('RGBA',logo_size) as logo:
     logo.paste(square1,(0,0))
-    logo.paste(square2,(size[0],0))
-    logo.paste(square3,(size[0]*2,0))
+    logo.paste(square2,(sub_size[0],0))
+    logo.paste(square3,(sub_size[0]*2,0))
     logo.save('logo.png')
 square1.close()
 square2.close()
