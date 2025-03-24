1dc8ea9fec9110993821ec24af0022c76a72f957
barnabeepickle <linusrk30@gmail.com>
2025-03-24 08:18:20 -0400

    obliterated argparse

--- breaker_cli.py
+++ breaker_cli.py
@@ -6,40 +6,9 @@ import pathlib
 
 # vars
 default_size=1024*1024*50
+mode_key={'break':0,'rebuild':1,'gen':2}
 
-# argparse setup
-mode_key={'break':0,'rebuild':1,'igen':2}
-# setup the program/parser/wrapper
-parser=argparse.ArgumentParser( # most of this is just taken directly from the documentation
-                    prog='file-breaker-cli',
-                    usage='%(prog)s [options]', # smarter code than I, now don't quote me on that, 
-                    # maybe quote me on that, this code comment has gone on long enough!
-                    description='A CLI interface for breaking files into segments.',
-                    epilog='"AS IS", WITHOUT WARRANTY')
-# add the arguments
-parser.add_argument('input_path',
-                    type=pathlib.Path,
-                    help='Path to the input file.')
-parser.add_argument('mode',
-                    type=str,
-                    choices=['break','rebuild','igen'],
-                    help='If the input file should be broken or reassembled',
-                    default='break')
-parser.add_argument('-s', '--size',
-                    type=int,
-                    help='Specify the size in bytes of the output file.',
-                    default=default_size)
-parser.add_argument('-c','--csv',
-                    help='Disable the need for a CSV file (generates index on demand).',
-                    action='store_true')
-# parse the arguments
-path=parser.parse_args(args=['input_path'])
-mode=parser.parse_args(args=['mode']) # this doesn't work for some damn reason
-mode=mode_key[mode]
-size=parser.parse_args(args=['-s','--size'])
-# CSV has two uses but it is not fully implemented yet TODO: finish that
-csv=parser.parse_args(args=['-c','--csv'])
-# i don't know how to write good argparse .parse_args() code, like i really have no clue
+# argparse has been sent to the shadow realm
 
 # code
 if mode==1 and csv==True:
